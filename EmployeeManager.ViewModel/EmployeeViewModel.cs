a74e911058a8e8189288f1eb92895c34beedb92e
Josef Di Lisa <gdilisa@hotmail.com>
2022-01-12 12:48:12 +0100

    Added controll

---
+++ EmployeeManager.ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,78 @@
+﻿using EmployeeManager.Common.Model;
+using System;
+using System.ComponentModel;
+
+namespace EmployeeManager.ViewModel
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private readonly Employee employee;
+
+        public EmployeeViewModel(Employee employee)
+        {
+            this.employee = employee;
+        }
+
+
+        public string FirstName
+        {
+            get => employee.FirstName;
+            set
+            {
+                if (employee.FirstName != value)
+                {
+                    employee.FirstName = value;
+                    RaisePropertyChanged(nameof(FirstName));
+                }
+            }
+        }
+
+        public DateTimeOffset EntryDate
+        {
+            get => employee.EntryDate;
+            set
+            {
+                if (employee.EntryDate != value)
+                {
+                    employee.EntryDate = value;
+                    RaisePropertyChanged(nameof(EntryDate));
+                }
+            }
+        }
+
+        public int JobRoleId
+        {
+            get => employee.JobRoleId;
+            set
+            {
+                if (employee.JobRoleId != value)
+                {
+                    employee.JobRoleId = value;
+                    RaisePropertyChanged(nameof(JobRoleId));
+                }
+            }
+        }
+
+        public bool IsCoffeeDrinker
+        {
+            get => employee.IsCoffeeDrinker;
+            set
+            {
+                if (employee.IsCoffeeDrinker != value)
+                {
+                    employee.IsCoffeeDrinker = value;
+                    RaisePropertyChanged(nameof(IsCoffeeDrinker));
+                }
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
